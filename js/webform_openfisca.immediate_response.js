1ef3e6cd7b3e84f16c362a9814c7873ffd0ff882
Sonny Kieu <39135-sonnykt@users.noreply.drupalcode.org>
2024-12-26 23:38:44 +0000

    Added Drupal Gitlab CI.

--- js/webform_openfisca.immediate_response.js
+++ js/webform_openfisca.immediate_response.js
@@ -11,14 +11,18 @@
    *
    * @param {HTMLElement} element
    *   The element.
-   * @param {string} event_type
+   * @param {string} eventType
    *   The event type.
    */
-  function preserveEventHandler(element, event_type) {
-    let events = $._data(element, 'events');
-    if (typeof events !== 'undefined' && (event_type in events) && events[event_type].length) {
-      events[event_type].forEach((event) => {
-        $(element).bind('fiscaImmediateResponse:continue', event.handler)
+  function preserveEventHandler(element, eventType) {
+    const events = $._data(element, 'events');
+    if (
+      typeof events !== 'undefined' &&
+      eventType in events &&
+      events[eventType].length
+    ) {
+      events[eventType].forEach((event) => {
+        $(element).on('fiscaImmediateResponse:continue', event.handler);
       });
     }
   }
@@ -26,61 +30,98 @@
   Drupal.behaviors.webform_openfisca_immediate_response = {
     attach(context, settings) {
       // Redirect to result page upon Openfisca immediate response.
-      $.fn.webformOpenfiscaImmediateResponseRedirect = function (immediate_response) {
-        if (typeof immediate_response.confirmation_url !== 'undefined' && typeof immediate_response.query !== 'undefined') {
-          window.location = immediate_response.confirmation_url + '?' + immediate_response.query;
+      $.fn.webformOpenfiscaImmediateResponseRedirect = function (
+        immediateResponse,
+      ) {
+        if (
+          typeof immediateResponse.confirmation_url !== 'undefined' &&
+          typeof immediateResponse.query !== 'undefined'
+        ) {
+          window.location = `${immediateResponse.confirmation_url}?${immediateResponse.query}`;
         }
       };
 
       // Trigger the preserved events as immediate response has no result.
-      $.fn.webformOpenfiscaImmediateResponseContinue = function (triggering_element) {
-        if (triggering_element !== NULL) {
-          let element = $('[data-openfisca-webform-id=' + triggering_element.webform + '][data-openfisca-immediate-response=true][name=' + triggering_element.name + '][data-drupal-selector=' + triggering_element.selector + ']').get(0);
-          if (!element && triggering_element.original_selector !== '') {
+      $.fn.webformOpenfiscaImmediateResponseContinue = function (
+        triggeringElement,
+      ) {
+        if (triggeringElement !== null) {
+          let element = $(
+            `[data-openfisca-webform-id=${
+              triggeringElement.webform
+            }][data-openfisca-immediate-response=true][name=${
+              triggeringElement.name
+            }][data-drupal-selector=${triggeringElement.selector}]`,
+          ).get(0);
+          if (!element && triggeringElement.original_selector !== '') {
             // The selector may be changed but the element is not rebuilt.
             // Attempt to use the original_selector.
-            element = $('[data-openfisca-webform-id=' + triggering_element.webform + '][data-openfisca-immediate-response=true][name=' + triggering_element.name + '][data-drupal-selector=' + triggering_element.original_selector + ']').get(0);
+            element = $(
+              `[data-openfisca-webform-id=${
+                triggeringElement.webform
+              }][data-openfisca-immediate-response=true][name=${
+                triggeringElement.name
+              }][data-drupal-selector=${triggeringElement.original_selector}]`,
+            ).get(0);
           }
           if (!element) {
             // Attempt to use webform and element name instead.
-            element = $('[data-openfisca-webform-id=' + triggering_element.webform + '][data-openfisca-immediate-response=true][name=' + triggering_element.name + ']').get(0);
+            element = $(
+              `[data-openfisca-webform-id=${
+                triggeringElement.webform
+              }][data-openfisca-immediate-response=true][name=${
+                triggeringElement.name
+              }]`,
+            ).get(0);
           }
           $(element).triggerHandler('fiscaImmediateResponse:continue');
         }
       };
 
-      once('webformOpenfiscaImmediateResponse', '*[data-openfisca-immediate-response=true]', context).forEach(function (element) {
+      once(
+        'webformOpenfiscaImmediateResponse',
+        '*[data-openfisca-immediate-response=true]',
+        context,
+      ).forEach(function (element) {
         // Trigger fiscaImmediateResponse:request event.
-        const triggerFiscaImmediateResponse_request = function (event) {
+        const triggerFiscaImmediateResponseRequest = function (event) {
           // Only trigger on selected radio/checkbox.
-          if ($(this).is('input[type=radio]') || $(this).is('input[checkbox]')) {
-            if ($(this).is(':checked')) {
+          if (
+            this.matches('input[type=radio]') ||
+            this.matches('input[checkbox]')
+          ) {
+            if (this.matches(':checked')) {
               $(this).triggerHandler('fiscaImmediateResponse:request', event);
             }
           }
           // Or on non-empty form elements.
-          else if ($(this).val() !== "") {
+          else if (this.value !== '') {
             $(this).triggerHandler('fiscaImmediateResponse:request', event);
           }
-        }
+        };
 
         // Trigger the fiscaImmediateResponse:request event when the blur event
         // is triggered for text inputs and textareas.
-        if ($(element).is('input[type=text]') || $(element).is('textarea')) {
+        if (
+          element.matches('input[type=text]') ||
+          element.matches('textarea')
+        ) {
           // Postpone the default blur event handler.
           preserveEventHandler(element, 'blur');
-          $(element).unbind('blur')
-            .bind('blur', element, triggerFiscaImmediateResponse_request);
+          $(element)
+            .off('blur')
+            .on('blur', element, triggerFiscaImmediateResponseRequest);
         }
         // Trigger the fiscaImmediateResponse:request event when the change
         // event is triggered for other inputs and selects.
-        else if ($(element).is('input') || $(element).is('select')) {
+        else if (element.matches('input') || element.matches('select')) {
           // Postpone the default change event handler.
           preserveEventHandler(element, 'change');
-          $(element).unbind('change')
-            .bind('change', element, triggerFiscaImmediateResponse_request);
+          $(element)
+            .off('change')
+            .on('change', element, triggerFiscaImmediateResponseRequest);
         }
       });
-    }
+    },
   };
 })(jQuery, Drupal, drupalSettings);
