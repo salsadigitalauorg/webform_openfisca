4b0b1c11beef60925c1de94828c51eafc3feca82
Suchi <gargsuchi@gmail.com>
2026-02-10 11:04:25 +1100

    Trying to fix the kernel tests.

--- tests/src/Kernel/WebformOpenFiscaBlockAccessKernelTest.php
+++ tests/src/Kernel/WebformOpenFiscaBlockAccessKernelTest.php
@@ -7,7 +7,9 @@ namespace Drupal\Tests\webform_openfisca\Kernel;
 use Drupal\block\BlockInterface;
 use Drupal\block_content\Entity\BlockContent;
 use Drupal\Core\Session\AnonymousUserSession;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
 
 /**
  * Kernel test for webform_openfisca_block_access and RAC paragraph detection.
@@ -58,11 +60,11 @@ class WebformOpenFiscaBlockAccessKernelTest extends BaseKernelTestCase {
     $block_plugin = $this->createBlockPluginMock('block_content:' . $block_content->uuid());
 
     $request = Request::create('/', 'GET', []);
-    $session = new AnonymousUserSession();
+    $session = new Session(new MockFileSessionStorage());
     $request->setSession($session);
     $this->container->get('request_stack')->push($request);
 
-    $result = \webform_openfisca_block_access($block_plugin, 'view', $session);
+    $result = \webform_openfisca_block_access($block_plugin, 'view', new AnonymousUserSession());
 
     $this->assertTrue($result->isForbidden(), 'Block with RAC paragraph and ID not in ?blocks= must be forbidden.');
     $this->assertTrue($result->getCacheContexts() !== NULL && in_array('url.query_args:blocks', $result->getCacheContexts(), TRUE));
