6db62d9db327d8bd56dd14ecdc099c968eacaa25
Suchi <gargsuchi@gmail.com>
2026-02-10 11:58:20 +1100

    Fixed eslint errors.

--- js/openfisca-checkbox-popup.js
+++ js/openfisca-checkbox-popup.js
@@ -1,9 +1,9 @@
 (function (Drupal, drupalSettings) {
   Drupal.behaviors.openFiscaCheckboxPopup = {
-    attach: function (context, settings) {
+    attach (context, settings) {
       // Ensure we only attach once per context.
       const checkboxes = context.querySelectorAll(
-        'input[name="third_party_settings[webform_openfisca][fisca_enabled]"]'
+        'input[name="third_party_settings[webform_openfisca][fisca_enabled]"]',
       );
 
       checkboxes.forEach(function (checkbox) {
@@ -13,16 +13,16 @@
           checkbox.addEventListener('change', function () {
             if (checkbox.checked) {
               Drupal.dialog(
-                '<p>Please be aware that submission data is automatically saved in Drupal unless this feature is disabled. Based on your use case, you\'ll need to consider the privacy implications of your webform and may need to disable saving of submissions.</p>',
+                "<p>Please be aware that submission data is automatically saved in Drupal unless this feature is disabled. Based on your use case, you'll need to consider the privacy implications of your webform and may need to disable saving of submissions.</p>",
                 {
                   title: 'Important Notice',
-                  width: 400
-                }
+                  width: 400,
+                },
               ).showModal();
             }
           });
         }
       });
-    }
+    },
   };
 })(Drupal, drupalSettings);
